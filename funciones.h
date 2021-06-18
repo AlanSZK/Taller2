c41441b3351aef47a950aee5e26e248857b794ee
Alan <alan.slazakc@utem.cl>
2021-06-18 18:09:44 -0400

    Añadido funciones + makefile

---
+++ funciones.h
@@ -0,0 +1,23 @@
+#ifndef FUNCIONES_H
+#define FUNCIONES_H
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <stdlib.h>
+#include <map>
+
+
+using namespace std;
+
+
+
+void saludo();
+void integrantes();
+std::vector<std::string> obtenerdatos(std::string linea);
+
+
+
+
+
+#endif
